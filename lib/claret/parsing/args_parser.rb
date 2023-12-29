1e3442fe071226fe52f16e497cf52e07cfd63395
Stephen Ierodiaconou <stevegeek@gmail.com>
2023-12-29 16:29:29 +0100

    Arg parser refactor

--- lib/claret/parsing/args_parser.rb
+++ lib/claret/parsing/args_parser.rb
@@ -10,36 +10,54 @@ module Claret
       end
 
       def parse
-        group_parts.map do |group|
-          group = group.reject { _1.blank? }
-
+        group_paren_parts_into_args.map do |group|
           first_part, second_part = group.first, group.last
           if group.size == 1 && arg_identifier?(first_part)
-            name = clean_arg_identifier(first_part)
-            debug "Arg with identifier: #{name}"
-            start_pos = first_part.start_pos + first_part.index(name)
-            end_pos = start_pos + name.size - 1
-            Arg.new(name, nil, first_part.literal, start_pos, end_pos)
+            create_arg_with_identifier(first_part)
           elsif group.size == 2 && arg_identifier?(second_part)
-            raise unless first_part.paren_group? || arg_type?(first_part)
-            name = clean_arg_identifier(second_part)
-            end_pos = second_part.start_pos + second_part.index(name) + name.size - 1
-            type = first_part.to_code
-            debug "Arg with identifier and type: #{name} - #{type}"
-            Arg.new(name, type, second_part.literal, first_part.start_pos, end_pos)
+            create_arg_with_identifier_and_type(first_part, second_part)
           elsif arg_type_identifier_pair?(first_part)
-            type_literal, name_literal, *_rest = first_part.split(" ")
-            raise unless arg_type?(type_literal)
-            name = clean_arg_identifier(name_literal)
-            type = type_literal.to_code
-            end_pos = name_literal.start_pos + name_literal.index(name) + name.size - 1
-            debug "Arg with identifier and type: #{name} - #{type}"
-            Arg.new(name, type, name_literal.literal, type_literal.start_pos, end_pos)
+            create_arg_with_identifier_and_type_pair(first_part)
           end
         end
       end
 
       private
+
+      def create_arg_with_identifier(literal)
+        name = clean_arg_identifier(literal)
+        debug "Arg with identifier: #{name}"
+        create_argument(name, nil, literal, arg_start_pos(literal, name), arg_end_pos(literal, name))
+      end
+
+      def create_arg_with_identifier_and_type(type_group, name_literal)
+        raise unless type_group.paren_group? || arg_type?(type_group)
+        name = clean_arg_identifier(name_literal)
+        type = type_group.to_code
+        debug "Arg with identifier and type: #{name} - #{type}"
+        create_argument(name, type, name_literal, type_group.start_pos, arg_end_pos(name_literal, name))
+      end
+
+      def create_arg_with_identifier_and_type_pair(literal)
+        type_literal, name_literal, *_rest = literal.split(" ")
+        raise unless arg_type?(type_literal)
+        name = clean_arg_identifier(name_literal)
+        type = type_literal.to_code
+        debug "Arg with identifier and type: #{name} - #{type}"
+        create_argument(name, type, name_literal, type_literal.start_pos, arg_end_pos(name_literal, name))
+      end
+
+      def create_argument(name, type, name_literal, start_pos, end_pos)
+        Arg.new(name, type, name_literal.literal, start_pos, end_pos)
+      end
+
+      def arg_start_pos(item, name)
+        item.start_pos + item.index(name)
+      end
+
+      def arg_end_pos(item, name)
+        item.start_pos + item.index(name) + name.size - 1
+      end
 
       def clean_arg_identifier(item)
         item.match(/^\s*([\w_@]+)\s*([=:]|\z)/).captures.first
@@ -58,25 +76,33 @@ module Claret
       end
 
       # it should group by comma
-      def group_parts
+      def group_paren_parts_into_args
         grouped = []
         @paren_group.each do |item|
-          debug "Grouping item: '#{item}'"
           if item.paren_literal? && item.include?(",")
             segments = item.split(",", -1) # -1 to include empty segments
             if grouped.empty?
-              grouped = segments.map { [_1] }
+              append_parts_as_args(grouped, segments)
             else
-              grouped[-1] << segments[0]
-              grouped.concat segments[1..].map { [_1] }
+              append_part_to_last_arg(grouped, segments[0])
+              append_parts_as_args(grouped, segments[1..])
             end
           elsif grouped.empty?
-            grouped = [[item]]
+            append_parts_as_args(grouped, [item])
           else
-            grouped[-1] << item
+            append_part_to_last_arg(grouped, item)
           end
         end
-        grouped
+        # Remove any blank argument sections
+        grouped.map { |group| group.reject { _1.blank? } }
+      end
+
+      def append_part_to_last_arg(grouped, segment)
+        grouped[-1] << segment
+      end
+
+      def append_parts_as_args(grouped, segments)
+        grouped.concat segments.map { [_1] }
       end
     end
   end
